1d1ec124f79dba8f3892b33eb3a904856af1f2b1
MarikFrostr <recruiterhrpartner4@gmail.com>
2021-07-24 18:52:35 +0300

    Функция strcmp

---
+++ strcmp.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/* Написаная функция lib_strcmp, берет строку и копирует ее*/
+int lib_strcmp(char * str, char * str1){
+    int i = 0;
+    int result;
+    while (str[i] || str1[i])
+    {
+      if (str[i] > str1[i]){
+        return result = 100;
+      } else if (str[i] < str1[i]) {
+        return result = -100;
+      }
+      i++;
+    }
+    return result = 0;
+}
+
+
+ /*Функция сравнивает выполнение lib_strcmp и strcmp */
+void compare (char * new_string, char * old_string){
+  if (lib_strcmp(new_string, old_string) == strcmp(new_string,old_string)) {
+    printf ("Функции работают одинаково\n");
+  } else {
+    printf("Есть отличие в работе функций:\n");
+  }
+}
+
+
+
+int main(){
+  char * first_string = "Hello word";
+  char * second_string = "Hello word";
+
+  compare(first_string, second_string);
+  return 0;
+}
